9f3eeb6574bfd1194491eca73ac82b36b1a68a32
Islamosama1 <islambaaly@gmail.com>
2024-10-30 00:55:14 +0300

    yh

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -63,6 +63,7 @@ export const authOptions: NextAuthOptions = {
           },
         })
 
+        console.log('exisitngAccount', exisitngAccount)
         if (exisitngAccount) return true
 
         await prisma.account.create({
